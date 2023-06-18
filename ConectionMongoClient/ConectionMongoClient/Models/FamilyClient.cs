3e834fea899ee29724f2cf10a5e31b14cb7c00e3
AndresTY <san.duve@hotmail.com>
2023-06-17 23:21:12 -0500

    feat(backend):  add connection DB

---
+++ ConectionMongoClient/ConectionMongoClient/Models/FamilyClient.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.IO;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+namespace ConectionMongoClient.Models
+{
+    public class FamilyClient
+    {
+        private dynamic dependientes;
+        public int FamilyCount { get; set; } 
+        public dynamic? Dependientes { 
+            get { return dependientes; } 
+            set { dependientes = JsonSerializer.Serialize(value); } 
+        }
+    }
+
+}
---
+++ ConectionMongoClient/ConectionMongoClient/Models/LaborClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace ConectionMongoClient.Models
+{
+    public class LaborClient
+    {
+        private dynamic Financial;
+        public Boolean IsEmployee { get; set; }
+        public String ? JobName { get; set; }
+        public String ? JobTitle { get; set; }
+        public int ? salary { get; set; }
+        public dynamic FinancialObligations {
+            get { return Financial; }
+            set { Financial = JsonSerializer.Serialize(value); }
+        }
+
+    }
+}
